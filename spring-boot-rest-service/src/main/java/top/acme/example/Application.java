3edc40053bd4defa1c4976ec0de3e73fe636746f
root <root@NI-PC.localdomain>
2019-04-03 15:09:08 +0800

    代码示例

---
+++ spring-boot-rest-service/src/main/java/top/acme/example/Application.java
@@ -0,0 +1,18 @@
+package top.acme.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	/**
+	 * 测试
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
